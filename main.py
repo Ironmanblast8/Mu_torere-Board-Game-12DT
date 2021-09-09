b190eca65d20aafa40b667ce368dc59f95894823
Ironmanblast8 <pickingemailsarehard@gmail.com>
2021-09-09 22:29:12 +1200

    Added the blue checker button
    The blue checker button is only 1 button at the moment in the wrong place, all it does is print clicked

--- main.py
+++ main.py
@@ -13,20 +13,48 @@ from PIL import Image, ImageTk
 #Intizaling the program as a function.
 def init():
 
+    global root
     #root = tk which is tkinter this will allow me to use root.geometry 
-    #to chosse the screen size and root.config to pick a background colour
+    #to chosse the screen size and root.config to pick a background colour aswell as title.
     
-    root = tk.Tk()  
+    root = tk.Tk() 
+   
 
+    root.title('Mū Tōrere')
     root.geometry('1366x768')
     root.config(bg='OliveDrab2')
-                  
-    boardimg = PhotoImage(file='Mū tōrere.png')
-    Label(root,image=boardimg).pack()
+        
+    boardimg = PhotoImage(file='Mū tōrere.png') #This is importing the image
+    Label(root,image=boardimg).pack() #creating the image as a label
 
+    checkers() #Will need to call functions here because of the mainloop
     root.mainloop() 
 
+
+def checkers():
+    global root
+    blue = 0
+    blueimg = []
+    bluepeice = []
+    canvas = tk.Canvas(root, bg='OliveDrab2', relief="flat", borderwidth=0, highlightthickness=0, width=0)
+    frame = tk.Frame(canvas, bg='OliveDrab2', relief="flat", borderwidth=0, highlightthickness=0, width=0)
+    while blue < 4:
+        bluecheck= Image.open("bluecircle50.png")
+        bluecheck = ImageTk.PhotoImage(bluecheck)
+        bluechecker_button = tk.Button(frame, image=bluecheck, compound="center", borderwidth=0,  bg='OliveDrab2', command=click)
+        bluechecker_button.pack()
+        blue += 1
+        bluepeice.append(blueimg)
+    canvas.create_window(0, 0, anchor='nw', window=frame)
+    canvas.update_idletasks()
+    canvas.configure(scrollregion=canvas.bbox('all'))
+    canvas.pack(fill='both', expand=True, side='bottom')
+    root.mainloop()
+
+def click():
+    print("clicked")
 
 #If the name of the program is called main it will run everything below.
 if __name__ == '__main__':
     init()
+    
